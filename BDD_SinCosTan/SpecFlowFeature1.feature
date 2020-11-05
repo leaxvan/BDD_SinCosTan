3873839ea8d195996701127aeed96c6299d39ed8
Lea <lea-01@gmx.at>
2020-11-05 10:28:59 +0100

    Add project files.

---
+++ BDD_SinCosTan/SpecFlowFeature1.feature
@@ -0,0 +1,17 @@
+﻿Feature: Trigonometrische Funktionen
+
+@mytag
+Scenario: Sinus
+	Given the first number PI
+	When sinus is taken
+	Then the rasult should be 0
+
+Scenario: Cosinus
+	Given the first number PI
+	When cosnius is taken
+	Then the rasult should be -1
+
+Scenario: Tangens
+	Given the first number PI
+	When tangens is taken
+	Then the rasult should be 0
---
+++ BDD_SinCosTan/TrigonometrischeFunktionenSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BDD_SinCosTan
+{
+    [Binding]
+    public class TrigonometrischeFunktionenSteps
+    {
+        double nr_pi, erg;
+
+        [Given(@"the first number PI")]
+        public void GivenTheFirstNumberPI()
+        {
+            nr_pi = Math.PI;
+        }
+        
+        [When(@"sinus is taken")]
+        public void WhenSinusIsTaken()
+        {
+            erg = Math.Sin(nr_pi);
+        }
+        
+        [When(@"cosnius is taken")]
+        public void WhenCosniusIsTaken()
+        {
+            erg = Math.Cos(nr_pi);
+        }
+        
+        [When(@"tangens is taken")]
+        public void WhenTangensIsTaken()
+        {
+            erg = Math.Tan(nr_pi);
+        }
+        
+        [Then(@"the rasult should be (.*)")]
+        public void ThenTheRasultShouldBe(int p0)
+        {
+            Assert.Equal(p0, erg, 5);
+        }
+    }
+}
